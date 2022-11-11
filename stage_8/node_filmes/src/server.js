d657c7f374c32e27c3f50ec8c51ad65903a5ceae
douglas <douglasantosilvaemail@gmail.com>
2022-11-11 16:29:29 -0300

    preparando backend para deploy

--- stage_8/node_filmes/src/server.js
+++ stage_8/node_filmes/src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 const { AppErrors } = require("./utils/AppErrors")
 const express = require("express")
 const { routes } = require("./routes")
@@ -28,5 +29,5 @@ app.use((error, request, response, next) => {
     })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is Connected Port ${PORT}`))
